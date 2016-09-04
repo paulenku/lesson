443bd49537f019fedc5b4cf1f35c3fb0afc9c305
Nikita Roman <bigman0roman@gmail.com>
2016-09-04 21:15:47 +0300

    orange version

---
+++ app/js/common.js
@@ -0,0 +1,209 @@
+$(function() {
+	$(".item-wrap").owlCarousel({
+		loop: true,
+		responsive:{
+			0:{
+				items:1,
+			},
+			480:{
+				items:1,
+			},
+			580:{
+				items:2,
+			},
+			768:{
+				items:2,
+			},
+			992:{
+				items:3,
+			},
+			1200:{
+				items:4,
+			}
+		}
+	});
+
+	$(".partners-wrap").owlCarousel({
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		nav: true,
+		navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive:{
+			0:{
+				items:1,
+			},
+			480:{
+				items:2,
+			},
+			768:{
+				items:3,
+			},
+			992:{
+				items:4,
+			},
+			1200:{
+				items:4,
+			}
+		}
+	});
+
+	$(".s-direct .item-vertical p").equalHeights();//авто-висота
+	$(".s-model .item p").equalHeights();//авто-висота
+	$(".s-koment .person-wrap .message p").equalHeights();//авто-висота
+
+	$(".mouse-icon").click(function() {
+		$("html, body").animate({
+			scrollTop : $(".s-adv").offset().top
+		}, 800);
+	});
+	
+	$(".s-adv").waypoint(function(){
+		$({blurRadius: 5}).animate({blurRadius: 0}, {
+			duration: 1200,
+			easing: 'swing',
+			step: function() {
+				$(".s-adv h3 span").css({
+					"-webkit-filter": "blur("+this.blurRadius+"px)",
+					"filter": "blur("+this.blurRadius+"px)"
+				});
+			}
+		});
+		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+		$(".s-adv h3 span").each(function() {
+			var tcount = $(this).data("count");
+			$(this).animateNumber({ number: tcount,
+				easing: 'easeInQuad',
+				"font-size": "1.8125em",
+				numberStep: comma_separator_number_step},
+				1200);
+		});
+		this.destroy();
+	}, {
+		offset: '70%'
+	});
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
+	});
+
+	$(".portfolio-item").each(function(e){
+
+		var th = $(this);
+
+		th.attr("href", "#portfolio-img-" + e)
+		.find('.portfolio-popup')
+		.attr("id", "portfolio-img-" + e);
+
+	});
+
+	$(".portfolio-item").magnificPopup();
+
+	$('.mfp-gallery').magnificPopup({
+		mainClass: 'mfp-zoom-in',
+		type: 'image',
+		tLoading: '',
+		gallery:{
+			enabled:true,
+		},
+		removalDelay: 300,
+		callbacks: {
+			beforeChange: function() {
+				this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+			},
+			open: function() {
+				$.magnificPopup.instance.next = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+				}
+				$.magnificPopup.instance.prev = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+				}
+			},
+			imageLoadComplete: function() { 
+				var self = this;
+				setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+			}
+		}
+	});
+	
+	$('.mfp-certificate').magnificPopup({
+		mainClass: 'mfp-zoom-in',
+		type: 'image',
+		tLoading: '',
+		gallery:{
+			enabled:true,
+		},
+		removalDelay: 300,
+		callbacks: {
+			beforeChange: function() {
+				this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+			},
+			open: function() {
+				$.magnificPopup.instance.next = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+				}
+				$.magnificPopup.instance.prev = function() {
+					var self = this;
+					self.wrap.removeClass('mfp-image-loaded');
+					setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+				}
+			},
+			imageLoadComplete: function() { 
+				var self = this;
+				setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+			}
+		}
+	});
+
+	$('.portfolio-item').magnificPopup({
+		mainClass: 'my-mfp-zoom-in',
+		removeDelay: 300,
+		type: 'inline',
+	})
+
+	// changin color fill swg
+	$('img.img-svg').each(function(){
+		var $img = $(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		$.get(imgURL, function(data) {
+		// Get the SVG tag, ignore the rest
+		var $svg = $(data).find('svg');
+
+		// Add replaced image's ID to the new SVG
+		if(typeof imgID !== 'undefined') {
+			$svg = $svg.attr('id', imgID);
+		}
+		// Add replaced image's classes to the new SVG
+		if(typeof imgClass !== 'undefined') {
+			$svg = $svg.attr('class', imgClass+' replaced-svg');
+		}
+
+		// Remove any invalid XML tags as per http://validator.w3.org
+		$svg = $svg.removeAttr('xmlns:a');
+
+		// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+		if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+			$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+		}
+
+		  // Replace image with new SVG
+		  $img.replaceWith($svg);
+
+		}, 'xml');
+
+	});
+
+
+});
